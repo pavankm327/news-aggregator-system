08ba0dabd888f5389919892689fd70d29128b875
Pavan Kumar <project.manager@amareatechsolutions.com>
2024-11-26 13:50:45 +0530

    Updated the console.php code to  run the job every five minutes

--- routes/console.php
+++ routes/console.php
@@ -3,15 +3,15 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-// use App\Jobs\FetchArticlesJob;
+use App\Jobs\FetchArticlesJob;
 
 // Artisan::command('inspire', function () {
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote')->hourly();
 
 Schedule::command('articles:fetch')
-    ->everyMinute()
+    ->everyFiveMinutes()
     ->runInBackground();
 
-// $apis = ['NewsAPI', 'The Guardian', 'New York Times'];
-// Schedule::job(new FetchArticlesJob($apis))->everySecond();
+$apis = ['NewsAPI', 'The Guardian', 'New York Times'];
+Schedule::job(new FetchArticlesJob($apis))->everyFiveMinutes();
